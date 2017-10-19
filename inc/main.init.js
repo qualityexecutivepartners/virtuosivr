9178d98de3e25f1641e8c88073c2a3ac9ad655fe
Marty OConnor <marty@boultoncreative.com>
2017-10-19 15:34:52 -0400

    initial flipbook commit

---
+++ inc/main.init.js
@@ -0,0 +1,128 @@
+window.flipbookSettings = {
+    "settings": {
+        "Title": "Flipbook",
+        "FlipbookUid": "f3e50b91a8",
+        "PageWidth": 1200,
+        "PageHeight": 1200,
+        "MobileImageMaxWidth": 900,
+        "MobileImageMaxHeight": 900,
+        "ThumbnailWidth": 140,
+        "ThumbnailHeight": 140,
+        "DownloadUrl": "inc\/pdf\/flipbook.pdf",
+        "NavbarMinHeight": 628,
+        "NavbarMinWidth": 414
+    },
+    "pages": {
+        "1": {
+            "links": [],
+            "videos": []
+        },
+        "2": {
+            "links": [],
+            "videos": []
+        },
+        "3": {
+            "links": [],
+            "videos": []
+        },
+        "4": {
+            "links": [],
+            "videos": []
+        },
+        "5": {
+            "links": [],
+            "videos": []
+        },
+        "6": {
+            "links": [],
+            "videos": []
+        },
+        "7": {
+            "links": [],
+            "videos": []
+        },
+        "8": {
+            "links": [],
+            "videos": []
+        },
+        "9": {
+            "links": [],
+            "videos": []
+        },
+        "10": {
+            "links": [],
+            "videos": []
+        },
+        "11": {
+            "links": [],
+            "videos": []
+        },
+        "12": {
+            "links": [],
+            "videos": []
+        },
+        "13": {
+            "links": [],
+            "videos": []
+        },
+        "14": {
+            "links": [],
+            "videos": []
+        },
+        "15": {
+            "links": [],
+            "videos": []
+        },
+        "16": {
+            "links": [],
+            "videos": []
+        },
+        "17": {
+            "links": [],
+            "videos": []
+        },
+        "18": {
+            "links": [],
+            "videos": []
+        },
+        "19": {
+            "links": [],
+            "videos": []
+        },
+        "20": {
+            "links": [
+                {
+                    "top": 990.80495726496,
+                    "left": 421.73401709402,
+                    "width": 193.43452991453,
+                    "height": 31.757264957265,
+                    "is_internal": false,
+                    "link": "mailto:info@virtuosivr.com"
+                }
+            ],
+            "videos": []
+        }
+    },
+    "content": {
+        "1": "LEARNING EXPERIENCE \f",
+        "2": "\f",
+        "3": "\f",
+        "4": "\f",
+        "5": "WHERE PRACTICAL REALITY MEETS VIRTUAL REALITY! Now perform the full hand washing procedure \f",
+        "6": "Needle Change Intervention SOP Checklist: \u2022 Sanitize Gloves \u2022 Perform Line Clearance \u2022 Clamp Tubing to Prevent Drips \u2022 Remove Tubing and Discard \u2022 Cleanup Spills and Sanitize \u2022 Install New Components \u2022 Cleanup Spills and Sanitize KNOWLEDGE INFORMATION E X P E R I E N C E Needle Change Intervention SOP Checklist: \u2022 Sanitize Gloves \u2022 Perform Line Clearance \u2022 Clamp Tubing to Prevent Drips \u2022 Remove Tubing and Discard \u2022 Cleanup Spills and Sanitize \u2022 Install New Components \u2022 Cleanup Spills and Sanitize \f",
+        "7": "Needle Change Intervention SOP Checklist: \u2022 Sanitize Gloves \u2022 Perform Line Clearance \u2022 Clamp Tubing to Prevent Drips \u2022 Remove Tubing and Discard \u2022 Cleanup Spills and Sanitize \u2022 Install New Components \u2022 Cleanup Spills and Sanitize Needle Change Intervention SOP Checklist: \u2022 Sanitize Gloves \u2022 Perform Line Clearance \u2022 Clamp Tubing to Prevent Drips \u2022 Remove Tubing and Discard \u2022 Cleanup Spills and Sanitize \u2022 Install New Components \u2022 Cleanup Spills and Sanitize APPLICATION PROFICIENCY L E A R N I N G \f",
+        "8": "\f",
+        "9": "THE REALITY OF VIRTUAL REALITY Experience Success Experience Failure Experience Limited Downtime Experience on Your Schedule Gain Confidence Understand Why Practice, Practice, Practice Without Travel or Expenses EXPERIENCE LEARNING EMBED LEARNING Expectation 2 sec yes 15 sec 8 sec 10 sec Actual 13 sec yes 18 sec 11 sec 11 sec Status Steps Required 1. Wet hands 2. Get soap 3. Lather hands 4. Rinse hands 5. Dry hands Handwashing Process Check Remember! Sequence and timing of steps Use aseptic techniques Retry Immersive Continue \f",
+        "10": "EPISODES STERILE PRODUCTS AND \u2022 Industrial Microbiology \u2022 Aseptic Technique & Behavior in Laboratories \u2022 Microbiology Laboratory Practices \u2022 Sampling and Sample Management \u2022 Reagents and Media Management \u2022 Microbiology Data Evaluation and Reporting \u2022 Microbiology Method Suitability \u2022 Microbiological Investigations \u2022 Sterility Testing \u2022 Bacterial Endotoxin Testing \u2022 Microbial Limits Testing \u2022 Preservatives and Antimicrobial Effectiveness \u2022 Microbial ID and Culture Maintenance \u2022 Sterilization Indicator Control and Usage \u2022 Sterility Assurance \u2022 Aseptic Technique & Behavior in Manufacturing \u2022 Gowning for Classified Environments \u2022 Air Flow Pattern Studies \u2022 Facility Cleaning and Disinfection \u2022 Environmental Monitoring \u2022 Manufacturing Equipment Use Cycle \u2022 Equipment Cleaning \u2022 Sterilization Process Control \u2022 Terminal Sterilization Process Control \u2022 Sterile Filtration Process Control \u2022 Depyrogenation Process Control \u2022 Isolator Process Control \u2022 Formulation of Bulk Product \u2022 Aseptic Process Simulation \u2022 CCIT and Visual Inspection \f",
+        "11": "IMMERSIVES MICROBIOLOGY CURRICULA \u2022 Filling Interventions - Fallen Vial & Needle Change \u2022 Environmental Monitoring Sampling Plan Development on the Plant Floor \u2022 Facility Cleaning and Disinfection Techniques \u2022 Making Aseptic Connections \u2022 Visual Inspection \u2022 Microbial Limits Testing Setup and Technique \u2022 Microbiological Streaking for Isolation \u2022 Gram Stain Technique\u00a0 \u2022 Serial Dilution Technique and Use \u2022 BET Solution & Sample Preparation \u2022 Pipette Use \u2022 Vertical and Horizontal Unidirectional Air Flow \u2022 Hand Washing \u2022 Surface Sanitization and Wiping Techniques \u2022 Environmental Monitoring Sampling (RODAC & Swab) \u2022 Sterility Testing - Vials, Syringes, and Bottles \f",
+        "12": "E D U C A T I O N A L SCIENTIFIC AND TECHNICAL COURSES methodically developed and dynamically delivered PROCESS CHECKS to verify and engage learning during the experience Needle Change Intervention SOP Checklist: \u2022 Sanitize Gloves \u2022 Perform Line Clearance \u2022 Clamp Tubing to Prevent Drips \u2022 Remove Tubing and Discard \f",
+        "13": "C O M P O N E N T S IMMERSIVE VIRTUAL REALITY EXPERIENCES to experience success, gain confidence, experience failure, and embed learning KNOWLEDGE ASSESSMENTS to verify transfer of knowledge, application, and learning \f",
+        "14": "F U T U R E T E C H N O LO G Y S E R I E S : \f",
+        "15": "MORE OPTIONS \u2022 On-Site Performance Certification \u2022 Customized Virtual Reality Modules \u2022 Customized SOP using Virtual Reality \u2022 System Interfacing \u2022 Combination Products\/Drug Delivery\/ Manufacturing & Device Testing \u2022 Active Pharmaceutical Ingredients\/ Manufacturing & In-process Controls\/Testing \u2022 Solid Dose Products\/ Manufacturing & Chemistry \u2022 Biotechnology Products\/ Manufacturing & Associated Sciences \f",
+        "16": "Conceptualized, Designed, Developed, and Delivered by the Virtuosi of Quality Executive Partners AND THE BOLD REVOLUTION CONTINUES... \f",
+        "17": "\f",
+        "18": "\f",
+        "19": "\f",
+        "20": "V I R T U O S I V R . C O M info@virtuosivr.com | 216.538.6001 \f"
+    }
+}
